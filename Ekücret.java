d6fe453b6aa9565eaeec917d6e40dfd9e0167524
tolgakocakaplan <tolgakocakaplan@outlook.com>
2026-02-09 17:51:34 +0300

    Add files via upload

---
+++ Ekücret.java
@@ -0,0 +1,17 @@
+public class ÖRNEK3 {
+    public static void main(String[] args)
+    {
+        String ad ="Ahmet";
+        String soyad ="Kuntman";
+        String adres ="Antalya";
+        String telefon = "05522984725";
+        String eposta = "xyz@gmail.com";
+        String dogumt ="18081991";
+        FLOAT aylikmaas = 100000;
+        double ekucret= (aylikmaas + aylikmaas*0.10);
+        System.out.println("Toplam Maaş="+ aylikmaas);
+
+
+
+    }
+}
---
+++ Kalan Maaş Hesaplama.java
@@ -0,0 +1,22 @@
+public class yeni2 {
+    public static void main(String[] args)
+    {
+        String ad ="Ahmet";
+        String soyad ="Kuntman";
+        int aylikmaas = 100000;
+        double gelirvergisi=   (aylikmaas*0.03);
+        double emlakvergisi=   (aylikmaas*0.02);
+        double barinmavergisi= (aylikmaas*0.05);
+        double kalanmaas = aylikmaas-(gelirvergisi+emlakvergisi+barinmavergisi);
+        System.out.println("Toplam Maaş="+ kalanmaas);
+
+
+
+
+
+
+
+
+
+    }
+}
